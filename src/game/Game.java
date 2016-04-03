4dd99c97af2ae5bd220e97d71f26d8b3722bdf92
Ivaylo Hristov <megapr0n94@gmail.com>
2016-04-03 17:22:25 +0300

    Added background animation

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,12 @@
 package game;
 
+import ImageLoader.SpriteSheet;
+import ImageLoader.gfx;
 import display.Display;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.io.IOException;
 
 public class Game implements Runnable {
     private String title;
@@ -16,30 +19,38 @@ public class Game implements Runnable {
     private Display display;
     private BufferStrategy bufferStrategy;
     private Graphics graphics;
+    private SpriteSheet background;
+    private int i;
 
     public Game(String title, int width, int height) {
         this.title = title;
         this.width = width;
         this.height = height;
         this.isRunning = false;
-
+        this.i = 23;
     }
 
     /**
      * Initializes all the graphics and it will get
      * everything ready for our game
      */
-    private void init() {
+    private void init() throws IOException {
         //Initializing a new Display
         this.display = new Display(title, width, height);
+        this.background = new SpriteSheet(gfx.loader("/images/RoadTile3.png"));
     }
     // Method for updating all the variables in the game
     private void tick() {
-
+        this.i--;
+        if (this.i == 0){
+            this.i = 23;
+        }
     }
     //Method for drawing everything on the canvas
-    private void render() {
-        //Setting the bufferStrategy to be the one used in our canvas
+    private void render() throws IOException {
+        // Bool is for determing wich background to draw.
+        boolean isFirstBckg = true;
+        //Setting the bufferStrategy to be the one used in our canvas.
         //Gets the number of buffers that the canvas should use.
         this.bufferStrategy = this.display.getCanvas().getBufferStrategy();
         //If our bufferStrategy doesn't know how many buffers to use
@@ -51,6 +62,8 @@ public class Game implements Runnable {
         }
         //Create the graphics related to the bufferStrategy
         graphics= this.bufferStrategy.getDrawGraphics();
+        //Create and draw the animated background
+        this.graphics.drawImage(this.background.crop(0, 0+this.i*this.height , width, height), 0, 0, null);
         //Enables the buffer
         bufferStrategy.show();
         //Shows everything stored in the Graphics object
@@ -59,11 +72,24 @@ public class Game implements Runnable {
     //Implementing the interface's method
     @Override
     public void run() {
-        init();
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         while (isRunning) {
+            try {
+                thread.sleep(13);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             tick();
-            render();
+            try {
+                render();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         //Calls the stop method to stop everything
         stop();
