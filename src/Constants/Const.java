af4dad9b98eda7d02014dadab6a1725e81ba4a67
plamenkoynov90 <plamen.koynov90@gmail.com>
2016-04-17 09:12:05 +0300

    Change some of the Constants and write comments

--- src/Constants/Const.java
+++ src/Constants/Const.java
@@ -2,6 +2,9 @@ package Constants;
 
 import java.util.*;
 
+/**
+ * Class containing all constant variables for the game
+ */
 public class Const {
     public static final int PLAYER_VELOCITY = 5;
     public static final int PLAYER_START_POINT_X = 620;
@@ -10,12 +13,11 @@ public class Const {
     public static final int PLAYER_HEIGHT = 170;
     public static final int LIVES = 5;
 
-    public static final int ENEMY_VELOCITY =  2;
+    public static final int ENEMY_VELOCITY =  5;
     public static final int ENEMY_START_POINT_Y = -200;
     public static final int ENEMY_END_POINT_X = 700;
     public static final int ENEMY_WIDTH = 90;
     public static final int ENEMY_HEIGHT = 170;
-
 
     public static final List<Integer> SPAWN_POINTS = new ArrayList<>(Arrays.asList(85, 260, 430, 610));
     public static final boolean[] OCCUPIED_SPAWN_POINTS = new boolean[4];
@@ -24,9 +26,8 @@ public class Const {
     public static final int ROAD_BOTTOM_BORDER = 400;
     public static final int ROAD_TOP_BORDER = -5;
 
-
     public static final int FPS = 50;
-    public static final int DELAY = 2000;
+    public static final int DELAY = 3000;
     public static final int TOTAL_BACKGROUND_FRAMES = 23;
-    public static final long DRAWING_DELAY = 400000;
+    public static final long DRAWING_DELAY = 500000;
 }
